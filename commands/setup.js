005a8fc67f21f844b275612147a4e9c17b813a98
Althael <46852680+AlthaelFR@users.noreply.github.com>
2019-03-07 13:47:01 +0100

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -11,7 +11,6 @@ exports.execute = async (client, message, args) => {
       .setDescription("~Installation du serveur~")
       .addField("Première étape:", "Mettre le role ReBot au dessus des membres")
       .addField("Seconde étape:", "Créer le salon ``📄logs`` il ne doit être visible que par le staff et ReBot")
-      .addField("Troisième étape:", "Choisis ton prefix en faisant le commande ``-prefix``\n**Pour plus d'aide vous pouvez toujours faire ``-help``.**")
       .setColor("#ffd800")
       .setImage("https://media.giphy.com/media/3d4QLdc76xEhbQDZGC/giphy.gif")
       .setAuthor("ReBot - BOT FRANCAIS", bicon);
